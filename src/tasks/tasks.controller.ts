edc0780b2c9752471502c41090af8a01b009d5ad
DiegoMutre <tysonmutre10@gmail.com>
2021-07-04 19:19:08 -0500

    * Use tasks service on TasksController

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  // Inject tasks service
+  constructor(private tasksService: TasksService) {}
+}
