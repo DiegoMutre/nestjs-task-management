91c082445e855f3cb2804be091b6af15d7c751fe
DiegoMutre <tysonmutre10@gmail.com>
2021-07-05 16:08:38 -0500

    * Create getTasks method on task repository

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,11 +1,31 @@
 import { NotFoundException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDTO } from './dto/create-task.dto';
+import { GetTaskFilterDTO } from './dto/get-tasks-filters.dto';
 import { TaskStatus } from './tasks-status.enum';
 import { Task } from './tasks.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+    async getTasks(filterDTO: GetTaskFilterDTO) {
+        const { status, search } = filterDTO;
+        const query = this.createQueryBuilder('task');
+
+        if (status) {
+            query.andWhere('task.status = :status', { status }); // &&
+        }
+
+        if (search) {
+            query.andWhere(
+                'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+                { search: `%${search}%` }, // The `%search%` is to match either be incompleted or completed, example: `create` === `crea`
+            );
+        }
+
+        const tasks = await query.getMany();
+        return tasks;
+    }
+
     async createTask(createTaskDTO: CreateTaskDTO): Promise<Task> {
         const { title, description } = createTaskDTO; // This only works when the class is defined as a type
 
