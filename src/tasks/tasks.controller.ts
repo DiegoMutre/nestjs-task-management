54528e4c09e21bc3a84fd3a5d73b4189a88c283b
DiegoMutre <tysonmutre10@gmail.com>
2021-07-04 21:58:05 -0500

    * Create new post method 'createTask'

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -10,5 +10,13 @@ export class TasksController {
     @Get()
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+
+    @Post()
+    createTask(
+        @Body('title') title: string, // param : 'title' - arg name: title - arg type: string
+        @Body('description') description: string,
+    ): Task {
+        return this.tasksService.createTask(title, description);
     }
 }
