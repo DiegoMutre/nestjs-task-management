a85b16804c5ec72ddadb59cbb5e947f7a5db9ce6
DiegoMutre <tysonmutre10@gmail.com>
2021-07-04 19:22:48 -0500

    * Update eslint and prettierrc config
    * Format tasks.controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -3,6 +3,6 @@ import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-  // Inject tasks service
-  constructor(private tasksService: TasksService) {}
+    // Inject tasks service
+    constructor(private tasksService: TasksService) {}
 }
