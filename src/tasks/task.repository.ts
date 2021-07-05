3cf9c327d6a44627210f300adbef82f1036cf8d9
DiegoMutre <tysonmutre10@gmail.com>
2021-07-05 13:13:21 -0500

    * Create 'createTask' method on TaskRepository

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,5 +1,21 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { CreateTaskDTO } from './dto/create-task.dto';
+import { TaskStatus } from './tasks-status.enum';
 import { Task } from './tasks.entity';
 
 @EntityRepository(Task)
-export class TaskRepository extends Repository<Task> {}
+export class TaskRepository extends Repository<Task> {
+    async createTask(createTaskDTO: CreateTaskDTO): Promise<Task> {
+        const { title, description } = createTaskDTO; // This only works when the class is defined as a type
+
+        const task = this.create({
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        });
+
+        await this.save(task);
+
+        return task;
+    }
+}
