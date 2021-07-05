4db7984ad9fd9c897a9a84fc13900d663ce055d3
DiegoMutre <tysonmutre10@gmail.com>
2021-07-04 20:05:31 -0500

    * Create getAllTasks get endpoint

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
-@Controller('tasks')
+@Controller('tasks') // 'apiURL/tasks
 export class TasksController {
     // Inject tasks service
     constructor(private tasksService: TasksService) {}
+
+    @Get()
+    getAllTasks() {
+        return this.tasksService.getAllTasks();
+    }
 }
