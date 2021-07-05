2247cee79423a05bc2507e046008465dd83df3e0
DiegoMutre <tysonmutre10@gmail.com>
2021-07-05 06:52:55 -0500

    * Create updateTaskStatus method on tasks service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -34,4 +34,10 @@ export class TasksService {
         this.tasks = this.tasks.filter(task => task.id !== id);
         return this.tasks;
     }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const task = this.getTaskById(id);
+        task.status = status;
+        return task;
+    }
 }
