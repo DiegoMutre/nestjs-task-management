70753682122b349ad3cf615b4eae3ff1d8067074
DiegoMutre <tysonmutre10@gmail.com>
2021-07-05 19:31:02 -0500

    * Create signUp method on auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
@@ -8,4 +9,8 @@ export class AuthService {
         @InjectRepository(UsersRepository)
         private usersRepository: UsersRepository,
     ) {}
+
+    signUp(authCredentialsDTO: AuthCredentialsDTO) {
+        return this.usersRepository.createUser(authCredentialsDTO);
+    }
 }
